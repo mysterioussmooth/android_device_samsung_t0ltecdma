bd65e3cf66caff0aa301ebd290ad1acce8207c2d
liquid0624 <rjbirnesser@gmail.com>
2013-02-15 10:21:45 -0500

    Readd 4.6 Kernel Toolchain Override
    Please do not remove this if you need to change it do it locally.
    Keeping this inplace till our smdk4412 kernel builds with 4.7
    for all the devices that are effected.

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -22,6 +22,7 @@ LOCAL_PATH := device/samsung/t0ltecdma
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := cyanogenmod_t0ltecdma_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.6
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
